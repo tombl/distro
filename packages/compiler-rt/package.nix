72e0e3b7107a5ffad90c2ba05b0163d0d2a94bfa
Thomas Stokes <git@tombl.dev>
2025-01-14 01:46:43 +0800

    (attempt to) Fix the failing compilation of clang's compiler-rt
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/tombl/distro?shareId=XXXX-XXXX-XXXX-XXXX).

--- packages/compiler-rt/package.nix
+++ packages/compiler-rt/package.nix
@@ -27,8 +27,18 @@ run
     cd llvm
 
     cmake -B build -G Ninja \
+      -DCMAKE_C_COMPILER_WORKS=ON \
+      -DCMAKE_CXX_COMPILER_WORKS=ON \
       -DCMAKE_BUILD_TYPE=Release \
+      -DCOMPILER_RT_BAREMETAL_BUILD=On \
+      -DCOMPILER_RT_BUILD_XRAY=OFF \
+      -DCOMPILER_RT_INCLUDE_TESTS=OFF \
+      -DCOMPILER_RT_HAS_FPIC_FLAG=OFF \
+      -DCOMPILER_RT_ENABLE_IOS=OFF \
+      -DCOMPILER_RT_DEFAULT_TARGET_ONLY=On \
+      -DLLVM_CONFIG_PATH=$ROOT_DIR/build/llvm/bin/llvm-config \
       -DCMAKE_INSTALL_PREFIX=$out \
+      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
       -DLLVM_ENABLE_RUNTIMES="compiler-rt" \
       -DLLVM_TARGETS_TO_BUILD="WebAssembly" \
       -DLLVM_USE_LINKER=lld
