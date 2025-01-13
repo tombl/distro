9512e1a7a46baf993237ad535fdd077aab023ef0
Thomas Stokes <git@tombl.dev>
2025-01-14 01:10:50 +0800

    add ci

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     # repo meta:
     # keep-sorted start block=yes
@@ -59,14 +62,28 @@
       ];
 
       perSystem =
-        { pkgs, system, ... }:
         {
-          packages = import ./all-packages.nix {
+          pkgs,
+          system,
+          config,
+          lib,
+          ...
+        }:
+        {
+          # For better or for worse, nixpkgs has established the pattern of
+          # a legacyPackages attribute that does not contain legacy packages at all,
+          # but rather an attribute set that's just not the shape of the typical packages attribute.
+          # In our case, we have a handful of non-package attributes that we still want to expose under the pkgs object.
+          legacyPackages = import ./all-packages.nix {
             inherit (inputs.nixpkgs) lib;
             inherit inputs;
             currentSystem = system;
             hostpkgs = import ./host-packages.nix { inherit pkgs; };
           };
+
+          # and then expose a filtered version of that attribute set with just the actual packages.
+          packages = lib.filterAttrs (_name: value: value ? drvPath) config.legacyPackages;
+          checks = config.packages;
         };
     };
 }
--- treefmt.nix
+++ treefmt.nix
@@ -2,6 +2,7 @@
   projectRootFile = "flake.nix";
 
   # keep-sorted start
+  programs.actionlint.enable = true;
   programs.keep-sorted.enable = true;
   programs.nixfmt.enable = true;
   # keep-sorted end
