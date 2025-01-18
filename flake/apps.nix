922d40265eefc9da2625430050857b744a41d52e
Thomas Stokes <git@tombl.dev>
2025-01-18 18:46:56 +0800

    refactor apps to shared package

--- flake/apps.nix
+++ flake/apps.nix
@@ -1,39 +1,32 @@
-{lib, ...}:
+{ lib, ... }:
 
 {
   perSystem =
-    { pkgs, self', ... }:
+    { pkgs, self', config, ... }:
     let
       inherit (self'.legacyPackages) linux initramfs;
+      site = pkgs.runCommand "wasm-linux" { src = "${linux.src}/tools/wasm"; } ''
+        mkdir $out
+        cp -r $src/run.js $src/public/* $src/src $out/
+        ln -s ${initramfs} $out/initramfs.cpio
+        ln -sf ${linux} $out/dist
+      '';
     in
     {
-      apps.runner.program =
-        let
-          runner-lib = pkgs.runCommand "wasm-linux-runner-lib" { src = "${linux.src}/tools/wasm"; } ''
-            mkdir -p $out/bin
+      apps.runner.program = pkgs.writeShellScriptBin "wasm-linux-runner" ''
+        ${lib.getExe pkgs.deno} run --allow-read ${site}/run.js --initcpio=${initramfs} "$@"
+      '';
 
-            ln -s ${linux} $out/dist
-            cp -r $src/src $out/src
-            cp $src/run.js $out      
-          '';
-        in
-        pkgs.writeShellScriptBin "wasm-linux-runner" ''
-          ${lib.getExe pkgs.deno} run --allow-read ${runner-lib}/run.js --initcpio=${initramfs} "$@"
-        '';
-
-        apps.serve.program =
-          let site = pkgs.runCommand "wasm-linux-site" { src = "${linux.src}/tools/wasm"; } ''
-            mkdir $out
-            cp -r $src/public/* $out/
-            ln -s ${initramfs} $out/initramfs.cpio
-            ln -sf ${linux} $out/dist
-          '';
-        in
-        pkgs.writeShellScriptBin "wasm-linux-serve" ''
-          ${lib.getExe pkgs.miniserve} ${site} --index index.html \
-            --header Cross-Origin-Opener-Policy:same-origin \
-            --header Cross-Origin-Embedder-Policy:require-corp \
-            --header Cross-Origin-Resource-Policy:cross-origin
-        '';
+      apps.serve.program = pkgs.writeShellScriptBin "wasm-linux-serve" ''
+        ${lib.getExe pkgs.miniserve} ${site} --index index.html \
+          --header Cross-Origin-Opener-Policy:same-origin \
+          --header Cross-Origin-Embedder-Policy:require-corp \
+          --header Cross-Origin-Resource-Policy:cross-origin
+      '';
+  
+      make-shells.default.packages = [
+        (pkgs.writeShellScriptBin "run" ''exec ${config.apps.runner.program} "$@"'')
+        (pkgs.writeShellScriptBin "serve" ''exec ${config.apps.serve.program} "$@"'')
+      ];
     };
 }
--- flake/shell.nix
+++ flake/shell.nix
@@ -5,7 +5,12 @@
   perSystem =
     { pkgs, ... }:
     {
+      make-shells.default = {
+        stdenv = pkgs.stdenvNoCC;
+      };
+
       make-shells.ci = {
+        stdenv = pkgs.stdenvNoCC;
         packages = with pkgs; [
           attic-client
           jq
