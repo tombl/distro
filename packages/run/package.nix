820589b13482c30289580a28f5a04c1e9ccc99a2
Thomas Stokes <git@tombl.dev>
2025-01-18 11:39:22 +0800

    run: split into separate file

---
+++ packages/run/package.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  currentSystem,
+  bash,
+  busybox,
+}:
+
+{
+  path ? [ ],
+  src ? null,
+  ...
+}@args:
+command:
+
+let
+  setup =
+    if src != null then
+      ''
+        cp -r ${src} src
+        cd src
+        chmod -R u+w .
+      ''
+    else
+      "";
+in
+
+derivation (
+  {
+    system = currentSystem;
+    builder = "${bash}/bin/bash";
+    args = [
+      "-euc"
+      ''
+        if [ 0 -eq "$NIX_BUILD_CORES" ]; then
+          NIX_BUILD_CORES=$(${busybox}/bin/nproc)
+        fi
+        exec ${bash}/bin/bash -eux $commandPath
+      ''
+    ];
+    PATH = lib.makeBinPath (
+      path
+      ++ [
+        bash
+        busybox
+      ]
+    );
+    command = setup + "\n" + command;
+    passAsFile = [ "command" ];
+  }
+  // (removeAttrs args [
+    "path"
+    "src"
+  ])
+)
