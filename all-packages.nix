046e3083cd95c9a94648ed9efb56f60ae3531872
Thomas Stokes <git@tombl.dev>
2025-01-18 11:39:22 +0800

    run: split into separate file

--- all-packages.nix
+++ all-packages.nix
@@ -6,50 +6,9 @@
 }:
 
 let
-  mkRun =
-    { path, builder }:
-    args: command:
-    let
-      setup =
-        if args ? src then
-          ''
-            cp -r ${args.src} src
-            cd src
-            chmod -R u+w .
-          ''
-        else
-          "";
-    in
-    derivation (
-      {
-        system = currentSystem;
-        inherit builder;
-        args = [
-          "-euc"
-          ''
-            if [ 0 -eq "$NIX_BUILD_CORES" ]; then
-              NIX_BUILD_CORES=$(${pkgs.busybox}/bin/nproc)
-            fi
-            exec ${builder} -eux $commandPath
-          ''
-        ];
-        PATH = lib.makeBinPath ((args.path or [ ]) ++ path);
-        command = setup + "\n" + command;
-        passAsFile = [ "command" ];
-      }
-      // (removeAttrs args [ "srcPath" ])
-    );
-
   wasmpkgs =
     {
-      inherit lib inputs;
-      run = mkRun {
-        path = [
-          pkgs.busybox
-          pkgs.bash
-        ];
-        builder = "${pkgs.bash}/bin/bash";
-      };
+      inherit lib inputs currentSystem;
     }
     // lib.packagesFromDirectoryRecursive {
       callPackage = lib.callPackageWith pkgs;
