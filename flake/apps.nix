09fdad17e0c89fd516215d1533745727fc48b9d9
Thomas Stokes <git@tombl.dev>
2025-01-18 19:02:53 +0800

    expose generated site as package

--- flake/apps.nix
+++ flake/apps.nix
@@ -2,28 +2,34 @@
 
 {
   perSystem =
-    { pkgs, self', config, ... }:
+    {
+      pkgs,
+      self',
+      config,
+      ...
+    }:
     let
       inherit (self'.legacyPackages) linux initramfs;
-      site = pkgs.runCommand "wasm-linux" { src = "${linux.src}/tools/wasm"; } ''
+    in
+    {
+      packages.site = pkgs.runCommand "wasm-linux" { src = "${linux.src}/tools/wasm"; } ''
         mkdir $out
         cp -r $src/run.js $src/public/* $src/src $out/
         ln -s ${initramfs} $out/initramfs.cpio
         ln -sf ${linux} $out/dist
       '';
-    in
-    {
+
       apps.runner.program = pkgs.writeShellScriptBin "wasm-linux-runner" ''
-        ${lib.getExe pkgs.deno} run --allow-read ${site}/run.js --initcpio=${initramfs} "$@"
+        ${lib.getExe pkgs.deno} run --allow-read ${config.packages.site}/run.js --initcpio=${initramfs} "$@"
       '';
 
       apps.serve.program = pkgs.writeShellScriptBin "wasm-linux-serve" ''
-        ${lib.getExe pkgs.miniserve} ${site} --index index.html \
+        ${lib.getExe pkgs.miniserve} ${config.packages.site} --index index.html \
           --header Cross-Origin-Opener-Policy:same-origin \
           --header Cross-Origin-Embedder-Policy:require-corp \
           --header Cross-Origin-Resource-Policy:cross-origin
       '';
-  
+
       make-shells.default.packages = [
         (pkgs.writeShellScriptBin "run" ''exec ${config.apps.runner.program} "$@"'')
         (pkgs.writeShellScriptBin "serve" ''exec ${config.apps.serve.program} "$@"'')
