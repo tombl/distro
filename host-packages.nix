01041c12a2e796f0ae4d97e8aa20f0c9e4a8a113
Thomas Stokes <git@tombl.dev>
2025-01-19 18:53:24 +0800

    ugh

--- host-packages.nix
+++ host-packages.nix
@@ -1,15 +1,18 @@
 # basic build dependencies from nixpkgs for cross compilation
 
-{ pkgs }:
+{ pkgs, wasmpkgs }:
 
 let
   llvm = pkgs.llvmPackages_19;
 in
 
 {
-  clang = llvm.clang-unwrapped.overrideAttrs (attrs: {
-    patches = attrs.patches or [ ] ++ [ ./packages/clang/add-wasm-linux-target.patch ];
-  });
+  clang-no-compiler-rt = llvm.clang-unwrapped;
+  clang = pkgs.runCommand "clang-with-wasm-compiler-rt" { } ''
+    mkdir -p $out/lib/clang/19/lib/wasm32-unknown
+    cp -r ${llvm.clang-unwrapped}/* $out/
+    cp ${wasmpkgs.compiler-rt}/libclang_rt.builtins-wasm32.a $out/lib/clang/19/lib/wasm32-unknown/libclang_rt.builtins.a
+  '';
   clang-host = llvm.clang;
   inherit (llvm) lld;
   inherit (pkgs)
