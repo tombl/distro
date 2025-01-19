f391d950201c81e44c2e7cf6aaf28ced9b7b326e
Thomas Stokes <git@tombl.dev>
2025-01-19 18:53:24 +0800

    ugh

--- packages/clang/package.nix
+++ packages/clang/package.nix
@@ -30,18 +30,21 @@ run
     ];
   }
   ''
-    (cd clang && patch -p1 <${./add-wasm-linux-target.patch})
+    patch -p1 <${./add-wasm-linux-target.patch}
 
     cmake -S llvm -B build -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_C_FLAGS="-I${musl}/include --target=wasm32-linux" \
-      -DCMAKE_CXX_FLAGS="-I${musl}/include --target=wasm32-linux" \
+      -DCMAKE_C_COMPILER_TARGET=wasm32-unknown \
+      -DCMAKE_CXX_COMPILER_TARGET=wasm32-unknown \
       -DCMAKE_INSTALL_PREFIX=$out \
+      -DCMAKE_SYSROOT=${musl} \
       -DCMAKE_SYSTEM_NAME=Linux \
+      -DLLVM_ENABLE_PROJECTS=clang \
+      -DLLVM_ENABLE_RUNTIMES="" \
       -DLLVM_HOST_TRIPLE=wasm32-linux \
-      -DLLVM_ENABLE_PROJECTS="clang;lldb" \
       -DLLVM_TARGET_ARCH=wasm32-linux \
-      -DLLVM_TARGETS_TO_BUILD="WebAssembly"
+      -DLLVM_TARGETS_TO_BUILD="WebAssembly" \
+      -DLLVM_USE_LINKER=lld
 
     cmake --build build --target install -j$NIX_BUILD_CORES
   ''
