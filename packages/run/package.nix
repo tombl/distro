93cc09e0ecc1e01b4690f5a88becf904bce5ec8c
Thomas Stokes <git@tombl.dev>
2025-02-23 17:21:51 +0800

    meta: add override mechanism
    now if you run `just eject linux`, you can edit the kernel source code
    in overrides/linux/src, then run `just build linux run` to incrementally
    rebuild the kernel then boot the system.

--- packages/run/package.nix
+++ packages/run/package.nix
@@ -23,7 +23,7 @@ let
           ''
             if [[ -v stdenv ]]; then
               source $stdenv/setup
-              build
+              buildPhase
             else
               source $commandPath
             fi
@@ -57,7 +57,19 @@ let
           fi
 
           eval "_build() { $(cat $commandPath); }"
-          build() {
+          buildPhase() {
+            if [[ "$name" =~ -env$ ]]; then
+              name="${"$"}{name%-env}"
+              if [ ! -d "overrides/$name/src" ]; then
+                echo "error: no override found for $name"
+                return 1
+              fi
+              cd overrides/$name/src
+              unset src
+              _build
+              return
+            fi
+
             if [[ -v src ]]; then
               cp -r $src src
               cd src
@@ -65,6 +77,10 @@ let
               unset src
             fi
             _build
+          }
+
+          runHook() {
+            "$1"
           }
         '';
         passAsFile = [ "setup" ];
--- treefmt.nix
+++
@@ -1,9 +0,0 @@
-{
-  projectRootFile = "flake.nix";
-
-  # keep-sorted start
-  programs.actionlint.enable = true;
-  programs.keep-sorted.enable = true;
-  programs.nixfmt.enable = true;
-  # keep-sorted end
-}
