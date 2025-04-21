a5f2da886f45b5229c19495dd904a4de507c6858
Thomas Stokes <git@tombl.dev>
2025-04-21 13:09:33 +0800

    busybox: only init config once (#51)
    so you can change it in an override

--- packages/busybox/package.nix
+++ packages/busybox/package.nix
@@ -48,61 +48,62 @@ run
       esac
     }
 
-    make defconfig
-    config STATIC y
-    config NOMMU y
-    config STATIC_LIBGCC n
-    config CROSS_COMPILER_PREFIX llvm-
-    config SYSROOT ${musl}
-    config EXTRA_CFLAGS '-nostdlib -isystem ${musl}/include -I${linux.headers}/include ${lib.optionalString config.debug "-g"} -matomics -mbulk-memory'
-    config EXTRA_LDFLAGS ${compiler-rt}/libclang_rt.builtins-wasm32.a
-    config EXTRA_LDLIBS c
+    if ! [ -f .config ]; then
+      make defconfig
+      config STATIC y
+      config NOMMU y
+      config STATIC_LIBGCC n
+      config CROSS_COMPILER_PREFIX llvm-
+      config SYSROOT ${musl}
+      config EXTRA_CFLAGS '-nostdlib -isystem ${musl}/include -I${linux.headers}/include ${lib.optionalString config.debug "-g"} -matomics -mbulk-memory'
+      config EXTRA_LDFLAGS ${compiler-rt}/libclang_rt.builtins-wasm32.a
+      config EXTRA_LDLIBS c
 
-    config BOOTCHARTD n
-    config CONSPY n
-    config CROND n
-    config CRONTAB n
-    config DEVMEM n
-    config FBSPLASH n
-    config FTPD n
-    config HDPARM n
-    config HEXEDIT n
-    config HTTPD n
-    config IFDOWN n
-    config IFUP n
-    config INETD n
-    config NC n
-    config NSENTER n
-    config SCRIPT n
-    config START_STOP_DAEMON n
-    config SWAPOFF n
-    config SWAPON n
-    config TCPSVD n
-    config TELNETD n
-    config TIME n
-    config TS n
-    config UDPSVD n
-    config WGET n
+      config BOOTCHARTD n
+      config CONSPY n
+      config CROND n
+      config CRONTAB n
+      config DEVMEM n
+      config FBSPLASH n
+      config FTPD n
+      config HDPARM n
+      config HEXEDIT n
+      config HTTPD n
+      config IFDOWN n
+      config IFUP n
+      config INETD n
+      config NC n
+      config NSENTER n
+      config SCRIPT n
+      config START_STOP_DAEMON n
+      config SWAPOFF n
+      config SWAPON n
+      config TCPSVD n
+      config TELNETD n
+      config TIME n
+      config TS n
+      config UDPSVD n
+      config WGET n
 
-    config SENDMAIL n
-    config REFORMIME n
-    config MAKEMIME n
-    config POPMAILDIR n
+      config SENDMAIL n
+      config REFORMIME n
+      config MAKEMIME n
+      config POPMAILDIR n
 
-    config INIT n
-    config LINUXRC n
+      config INIT n
+      config LINUXRC n
 
-    config RUNSV n
-    config RUNSVDIR n
-    config SVLOGD n
+      config RUNSV n
+      config RUNSVDIR n
+      config SVLOGD n
 
-    config HUSH_TICK n
+      config HUSH_TICK n
 
-    config HWCLOCK n
-    config RTCWAKE n
+      config HWCLOCK n
+      config RTCWAKE n
 
-    cat .config
-    make oldconfig
+      make oldconfig
+    fi
 
     make ${lib.optionalString config.debug "SKIP_STRIP=y"}
 
