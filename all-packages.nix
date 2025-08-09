62b8715789b8752269d757a742e09b8f885f9433
Thomas Stokes <git@tombl.dev>
2025-08-09 20:22:22 +0800

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/9305fe4e5c2a6fcf5ba6a3ff155720fbe4076569?narHash=sha256-tYBdgS56eXYaWVW3fsnPQ/nFlgWi/Z2Ymhyu21zVM98%3D' (2025-06-08)
      → 'github:hercules-ci/flake-parts/af66ad14b28a127c5c0f3bbb298218fc63528a18?narHash=sha256-pHYj8gUBapuUzKV/kN/tR3Zvqc7o6gdFB9XKXIp1SQ8%3D' (2025-08-06)
    • Updated input 'git-hooks':
        'github:cachix/git-hooks.nix/623c56286de5a3193aa38891a6991b28f9bab056?narHash=sha256-WUaIlOlPLyPgz9be7fqWJA5iG6rHcGRtLERSCfUDne4%3D' (2025-06-11)
      → 'github:cachix/git-hooks.nix/9c52372878df6911f9afc1e2a1391f55e4dfc864?narHash=sha256-c6yg0EQ9xVESx6HGDOCMcyRSjaTpNJP10ef%2B6fRcofA%3D' (2025-08-05)
    • Updated input 'git-hooks/flake-compat':
        'github:edolstra/flake-compat/0f9255e01c2351cc7d116c072cb317785dd33b33?narHash=sha256-kvjfFW7WAETZlt09AgDn1MrtKzP7t90Vf7vypd3OL1U%3D' (2023-10-04)
      → 'github:edolstra/flake-compat/9100a0f413b0c601e0533d1d94ffd501ce2e7885?narHash=sha256-CIVLLkVgvHYbgI2UpXvIIBJ12HWgX%2BfjA8Xf8PUmqCY%3D' (2025-05-12)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/9e83b64f727c88a7711a2c463a7b16eedb69a84c?narHash=sha256-v263g4GbxXv87hMXMCpjkIxd/viIF7p3JpJrwgKdNiI%3D' (2025-06-17)
      → 'github:nixos/nixpkgs/c2ae88e026f9525daf89587f3cbee584b92b6134?narHash=sha256-erbiH2agUTD0Z30xcVSFcDHzkRvkRXOQ3lb887bcVrs%3D' (2025-08-06)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/a05be418a1af1198ca0f63facb13c985db4cb3c5?narHash=sha256-eEy8cuS0mZ2j/r/FE0/LYBSBcIs/MKOIVakwHVuqTfk%3D' (2025-06-06)
      → 'github:numtide/treefmt-nix/1298185c05a56bff66383a20be0b41a307f52228?narHash=sha256-B%2B3g9%2B76KlGe34Yk9za8AF3RL%2BlnbHXkLiVHLjYVOAc%3D' (2025-08-06)

--- all-packages.nix
+++ all-packages.nix
@@ -7,41 +7,40 @@
 let
   inherit (lib) filterAttrs pipe;
   inherit (builtins) mapAttrs readDir;
-  pkgs =
-    {
-      inherit lib currentSystem;
-      config = {
-        debug = false;
-      };
-    }
-    // lib.packagesFromDirectoryRecursive {
-      callPackage = lib.callPackageWith (pkgs // hostpkgs);
-      directory = ./packages;
-    }
-    // (
-      let
-        isImpure = builtins ? currentSystem;
-        overrides = builtins.getEnv "DISTRO_OVERRIDES";
-      in
-      if isImpure && overrides != "" then
-        pipe overrides [
-          readDir
-          (filterAttrs (_name: type: type == "directory"))
-          (mapAttrs (name: _: /${overrides}/${name}/outputs))
-          (mapAttrs (
-            name: dir:
-            let
-              pkg = (mapAttrs (output: _: /${dir}/${output}) (readDir dir)) // {
-                type = "derivation";
-                src = /${overrides}/${name}/src;
-                outPath = pkg.out;
-              };
-            in
-            pkg
-          ))
-        ]
-      else
-        { }
-    );
+  pkgs = {
+    inherit lib currentSystem;
+    config = {
+      debug = false;
+    };
+  }
+  // lib.packagesFromDirectoryRecursive {
+    callPackage = lib.callPackageWith (pkgs // hostpkgs);
+    directory = ./packages;
+  }
+  // (
+    let
+      isImpure = builtins ? currentSystem;
+      overrides = builtins.getEnv "DISTRO_OVERRIDES";
+    in
+    if isImpure && overrides != "" then
+      pipe overrides [
+        readDir
+        (filterAttrs (_name: type: type == "directory"))
+        (mapAttrs (name: _: /${overrides}/${name}/outputs))
+        (mapAttrs (
+          name: dir:
+          let
+            pkg = (mapAttrs (output: _: /${dir}/${output}) (readDir dir)) // {
+              type = "derivation";
+              src = /${overrides}/${name}/src;
+              outPath = pkg.out;
+            };
+          in
+          pkg
+        ))
+      ]
+    else
+      { }
+  );
 in
 pkgs
