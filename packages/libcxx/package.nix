11b6505a01f6b93499d2542e801282d49611aa5a
Thomas Stokes <git@tombl.dev>
2025-11-09 20:38:12 +0800

    libcxx: simplify definition

--- packages/libcxx/package.nix
+++ packages/libcxx/package.nix
@@ -32,47 +32,21 @@ run
     ];
   }
   ''
-    # Make libunwind headers available for libc++abi
-    export CPLUS_INCLUDE_PATH="$PWD/libunwind/include''${CPLUS_INCLUDE_PATH:+:$CPLUS_INCLUDE_PATH}"
-    export C_INCLUDE_PATH="$PWD/libunwind/include''${C_INCLUDE_PATH:+:$C_INCLUDE_PATH}"
-
     cmake -S runtimes -B build -G Ninja \
       -DCMAKE_BUILD_TYPE=${if config.debug then "Debug" else "Release"} \
       -DCMAKE_INSTALL_PREFIX=$out \
       -DCMAKE_SYSROOT=${musl} \
       -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
       -DCMAKE_C_COMPILER_TARGET=wasm32-unknown-linux-musl \
-      -DCMAKE_C_COMPILER_WORKS=ON \
-      -DCMAKE_C_FLAGS="-I${musl}/include -I${linux.headers}/include" \
+      -DCMAKE_C_FLAGS="-I${linux.headers}/include" \
       -DCMAKE_CXX_COMPILER_TARGET=wasm32-unknown-linux-musl \
-      -DCMAKE_CXX_COMPILER_WORKS=ON \
-      -DCMAKE_CXX_FLAGS="-I${musl}/include -I${linux.headers}/include" \
-      -DLIBCXXABI_LIBUNWIND_INCLUDES_INTERNAL=libunwind/include \
+      -DCMAKE_CXX_FLAGS="-I${linux.headers}/include" \
       -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi" \
-      -DLLVM_ENABLE_PROJECTS="" \
-      -DLLVM_HOST_TRIPLE=wasm32-unknown-linux-musl \
-      -DLLVM_TARGETS_TO_BUILD="WebAssembly" \
-      -DLIBCXX_ENABLE_FILESYSTEM=ON \
-      -DLIBCXX_ENABLE_RTTI=ON \
       -DLIBCXX_ENABLE_SHARED=OFF \
-      -DLIBCXX_ENABLE_STATIC=ON \
-      -DLIBCXX_ENABLE_THREADS=ON \
-      -DLIBCXX_HAS_PTHREAD_API=ON \
-      -DLIBCXX_ENABLE_EXCEPTIONS=ON \
-      -DLIBCXX_ENABLE_MONOTONIC_CLOCK=ON \
       -DLIBCXX_HAS_MUSL_LIBC=ON \
       -DLIBCXX_USE_COMPILER_RT=ON \
       -DLIBCXXABI_ENABLE_SHARED=OFF \
-      -DLIBCXXABI_ENABLE_THREADS=ON \
-      -DLIBCXXABI_HAS_PTHREAD_API=ON \
-      -DLIBCXXABI_ENABLE_EXCEPTIONS=ON \
-      -DLIBCXXABI_USE_COMPILER_RT=ON \
-      -DLIBCXXABI_INSTALL_STATIC_LIBRARY=ON \
-      -DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_SHARED_LIBRARY=OFF \
-      -DLIBCXX_CXX_ABI=libcxxabi \
-      -DLIBCXXABI_USE_LLVM_UNWINDER=OFF \
-      -DLIBCXXABI_ENABLE_STATIC_UNWINDER=OFF \
-      -DLLVM_PATH=llvm
+      -DLIBCXXABI_USE_LLVM_UNWINDER=OFF
 
     cmake --build build --target install -j$NIX_BUILD_CORES
   ''
