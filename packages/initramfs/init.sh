cb55a66e1ac471daa393a03901f2379a2d4e4587
Thomas Stokes <git@tombl.dev>
2025-11-10 21:59:53 +0800

    initramfs: create symlinks at runtime
    boots way faster because the kernel initramfs
    extractor is slow

--- packages/initramfs/init.sh
+++ packages/initramfs/init.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/busybox sh
+
+/bin/busybox --install -s
 
 mkdir -p /dev /proc /sys
 mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 mount -t devtmpfs devtmpfs /dev
 
-exec /bin/sh
+exec sh
