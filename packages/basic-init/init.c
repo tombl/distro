5c320e73da8ee8000be4d0f719e91eeec3bfd217
Thomas Stokes <git@tombl.dev>
2026-05-09 16:17:48 +0800

    repro /proc/self/mem bug

--- packages/basic-init/init.c
+++ packages/basic-init/init.c
@@ -1,13 +1,18 @@
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <sched.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <sys/stat.h>
+#include <sys/mount.h>
 #include <sys/utsname.h>
 #include <unistd.h>
 #include <string.h>
 
 static int foo = 0;
+static char proc_mem_probe[] = "proc-self-mem-probe";
 
 int other(void *arg) {
         printf("hello from other thread: %d\n", foo);
@@ -71,6 +76,45 @@ int main(int argc, char *argv[]) {
         }
         
         free(cwd);
+
+        if (mkdir("/proc", 0555) == -1 && errno != EEXIST) {
+                perror("mkdir /proc");
+                return 1;
+        }
+
+        if (mount("proc", "/proc", "proc", 0, NULL) == -1 && errno != EBUSY) {
+                perror("mount proc");
+                return 1;
+        }
+
+        int memfd = open("/proc/self/mem", O_RDONLY);
+        if (memfd == -1) {
+                perror("open /proc/self/mem");
+                return 1;
+        }
+
+        char mem_probe_buf[sizeof(proc_mem_probe)] = {0};
+        if (lseek(memfd, (off_t)(uintptr_t)proc_mem_probe, SEEK_SET) == (off_t)-1) {
+                perror("lseek /proc/self/mem");
+                return 1;
+        }
+
+        ssize_t mem_probe_len = read(memfd, mem_probe_buf, sizeof(mem_probe_buf));
+        if (mem_probe_len == -1) {
+                perror("read /proc/self/mem");
+                return 1;
+        }
+
+        printf("proc self mem read len=%zd value='%s' expected='%s'\n",
+               mem_probe_len, mem_probe_buf, proc_mem_probe);
+
+        if (mem_probe_len != sizeof(proc_mem_probe) ||
+            memcmp(mem_probe_buf, proc_mem_probe, sizeof(proc_mem_probe)) != 0) {
+                fprintf(stderr, "incorrect /proc/self/mem read\n");
+                return 1;
+        }
+
+        close(memfd);
 
         printf("idling forever\n");
         for (;;)
